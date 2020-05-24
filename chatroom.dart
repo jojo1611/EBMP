230b2d072d53bbcd1ea49090536eefe533a802fe
jojo1611 <46118506+jojo1611@users.noreply.github.com>
2020-05-24 21:35:22 +0530

    Add files via upload

---
+++ chatroom.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'database.dart';
+class ChatRoom extends StatefulWidget {
+  @override
+  _ChatRoomState createState() => _ChatRoomState();
+}
+
+class _ChatRoomState extends State<ChatRoom> {
+DatabaseMethods databaseMethods = new DatabaseMethods();
+  createChatRoomAndStartChat(String username){
+    List<String> users = {};
+    databaseMethods.createChatRoom();
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Chat",style: TextStyle(
+          fontSize:30
+        ),),
+        backgroundColor: Colors.greenAccent,
+      ),
+    );
+  }
+}
